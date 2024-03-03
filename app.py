b0f9a370759ca50426ed96c1838a279b61ff3de6
MohnishBhardwaj2811 <160467618+MohnishBhardwaj2811@users.noreply.github.com>
2024-03-03 10:10:23 +0530

    Update app.py
    done!

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from flask import Flask , request , jsonify , render_template
+from flask import Flask, request, jsonify, render_template
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 # Set up Chrome options
 chrome_options = Options()
@@ -17,17 +16,16 @@ chrome_options.add_argument('--disable-images')  # Disable loading of images
 
 # Set up the Chrome webdriver with options
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.implicitly_wait(1)  # Set implicit wait to 5 seconds
-
+driver.implicitly_wait(1)  # Set implicit wait to 1 second
 
 def main(url):
     attempt = 1
     while True:
-        print("Attempted:"+str(attempt))
-        attempt +=1
+        print("Attempted:" + str(attempt))
+        attempt += 1
         driver.get(url)
         # Find elements directly without explicit waiting
-        download_elements = driver.find_elements(By.CLASS_NAME, 'dowload')
+        download_elements = driver.find_elements(By.CLASS_NAME, 'download')
 
         if download_elements:
             break
@@ -43,29 +41,21 @@ def main(url):
 
     return data
 
-    # Close the browser window
-    driver.quit()
+# Close the browser window
+# This should be placed outside the main function to ensure it gets called
+# even if an exception occurs.
+try:
+    app = Flask(__name__)
 
-app = Flask(__name__)
-
-#Home
-@app.route("/<user_id>")
-def get_home(user_id):
-    url = 'https://embtaku.pro/download?id=' + user_id #MjIwNjY3&typesub=Gogoanime-SUB&title=Tousouchuu%3A+Great+Mission+Episode+44'
-    print(user_id)
-    try:
-        data = main(url)
-        #data = dict_content(data)
-        return jsonify(data)
-    except:
-        return {"data" : "data not found" , "data_code": 404 }
-
-
-
-
-
-
-
-
-
-
+    # Home
+    @app.route("/<user_id>")
+    def get_home(user_id):
+        url = 'https://embtaku.pro/download?id=' + user_id
+        print(user_id)
+        try:
+            data = main(url)
+            return jsonify(data)
+        except:
+            return {"data": "data not found", "data_code": 404}
+    finally:
+        driver.quit()
